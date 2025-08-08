2afc74431c6c47f1a47d2c993f7fc2e64ef48922
Joao Pedro <jfonseca2003@gmail.com>
2025-08-08 11:21:20 -0400

    Update home.tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,7 +4,7 @@ import Infograph from "../components/sections/infograph";
 import { DocumentTitle } from "../utils/utils";
 
 export default function Home() {
-  DocumentTitle("Home | Danny Hagenlocker");
+  DocumentTitle("Home | Joao Pedro Dudziak Fonseca");
   return (
     <div className="mx-auto min-h-screen max-w-screen-xl px-6 md:px-12 lg:px-24">
       <div className="fixed inset-0 hidden max-h-screen md:block">
